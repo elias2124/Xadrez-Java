19bd2985dff5d1a224f07fd375b85af00695be10
Elias Aguiar <eliasaguiarsousa1999@gmail.com>
2022-09-21 17:46:20 -0300

    Lógica de Check

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,8 +17,7 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
-		
-		
+
 		while (true) {
 			try {
 
@@ -37,13 +36,11 @@ public class Program {
 				ChessPosition target = UI.readChessPosition(sc);
 
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-				
+
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
-				
-				
-				
+
 			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -16,7 +16,11 @@ public abstract class ChessPiece extends Piece {
 	public Color getColor() {
 		return color;
 	}
-	
+
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
+	}
+
 	protected boolean isThereOpponentPice(Position position) {
 		ChessPiece p = (ChessPiece) getBoard().piece(position);
 		return p != null && p.getColor() != color;
