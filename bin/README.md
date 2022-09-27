6977cb061028ce75eb8330d32b45c9bf1d483c72
Elias Aguiar <eliasaguiarsousa1999@gmail.com>
2022-09-27 10:05:44 -0300

    Fix - tratamento melhor para promoção

---
+++ bin/README.md
@@ -0,0 +1,18 @@
+# Projeto Chess--System
+
+> Desenvolvido por Elias Aguiar
+
+## O que aprendi?
+
+- Como usar o java completo e programação orientada a objeto
+
+- Como utilizar isso para fazer um simples jogo de xadrez
+
+## Executando a aplicação
+
+Execute o seguinte comando no git bash:
+
+```bash
+	./javaapplication/Program
+``
+
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -55,7 +54,7 @@ public class ChessMatch {
 	public ChessPiece getEnPassantVulnerable() {
 		return enPassantVulnerable;
 	}
-	
+
 	public ChessPiece getPromoted() {
 		return promoted;
 	}
@@ -130,7 +129,7 @@ public class ChessMatch {
 			throw new IllegalStateException("There is no piece to be promoted");
 		}
 		if (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
-			throw new InvalidParameterException("Invalid type for promotion");
+			return promoted;
 		}
 
 		Position pos = promoted.getChessPosition().toPosition();
